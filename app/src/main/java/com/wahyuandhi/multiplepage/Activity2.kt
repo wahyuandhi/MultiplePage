8d6e4778a5f10dea70ad5670c4870e81d45eea65
wahyuandhi <wakay.98@gmail.com>
2020-12-18 14:58:26 +0700

    Add ViewBinding + Intent Activity

--- app/src/main/java/com/wahyuandhi/multiplepage/Activity2.kt
+++ app/src/main/java/com/wahyuandhi/multiplepage/Activity2.kt
@@ -2,10 +2,19 @@ package com.wahyuandhi.multiplepage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wahyuandhi.multiplepage.databinding.Activity2Binding
 
 class Activity2 : AppCompatActivity() {
+
+    private lateinit var binding: Activity2Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_2)
+        binding = Activity2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvName.text = intent.getStringExtra("Name")
+        binding.tvAddress.text = intent.getStringExtra("Address")
+        binding.tvContact.text = intent.getStringExtra("Contact")
     }
 }
--- app/src/main/java/com/wahyuandhi/multiplepage/MainActivity.kt
+++ app/src/main/java/com/wahyuandhi/multiplepage/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.wahyuandhi.multiplepage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.wahyuandhi.multiplepage.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun save(view: View) {
+        val intent = Intent(this, Activity2::class.java).apply {
+            putExtra("Name", binding.etName.text.toString())
+            putExtra("Address", binding.etAddress.text.toString())
+            putExtra("Contact", binding.etContact.text.toString())
+        }
+
+        startActivity(intent)
     }
 }
